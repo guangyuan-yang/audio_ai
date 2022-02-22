eaf25b6257bbf4b7601e73c5fea9c57eb312dcb6
guangyuan-yang <97737096+guangyuan-yang@users.noreply.github.com>
2022-02-22 04:21:43 +0000

    simplify interface, lazy eval of computed properties

--- audio_features.py
+++ audio_features.py
@@ -20,11 +20,129 @@ class AudioFeatures:
 
         return feature_stats
 
-    def __init__(self, filename=None, sample_rate=22050, duration=29):
-        self.filename = os.path.abspath(os.path.expanduser(filename))
-        self.sample_rate = sample_rate
-        self.duration = duration
-        self.num_samples = sample_rate * duration
+    def __init__(self, filename=None,
+                 sample_rate=22050,
+                 time_start=0,
+                 duration=29,
+                 frame_length=2048,
+                 n_fft=2048,
+                 hop_length=512):
+        self._filename = os.path.abspath(os.path.expanduser(filename))
+        self._sample_rate = sample_rate
+        self._time_start = time_start
+        self._duration = duration
+        self._audio_signal = None
+        self._sample_start = None
+        self._sample_end = None
+        self._frame_length = frame_length
+        self._n_fft = n_fft
+        self._hop_length = hop_length
+
+    @property
+    def filename(self):
+        return self._filename
+
+    @property
+    def sample_rate(self):
+        return self._sample_rate
+
+    @sample_rate.setter
+    def sample_rate(self, new_rate):
+        self._sample_rate = new_rate
+        self._audio_signal = None
+        self._sample_start = None
+        self._sample_end = None
+
+    @sample_rate.deleter
+    def sample_rate(self):
+        self._sample_rate = None
+        self._audio_signal = None
+        self._sample_start = None
+        self._sample_end = None
+
+    @property
+    def audio_signal(self):
+        if self._audio_signal is None:
+            self._audio_signal = self.load_wav_file()[0]
+        return self._audio_signal
+
+    @property
+    def time_start(self):
+        return self._time_start
+
+    @time_start.setter
+    def time_start(self, new_time):
+        self._time_start = new_time
+        self._sample_start = None
+        self._sample_end = None
+
+    @time_start.deleter
+    def time_start(self):
+        self._time_start = None
+        self._sample_start = None
+        self._sample_end = None
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, new_duration):
+        self._duration = new_duration
+        self._sample_end = None
+
+    @duration.deleter
+    def duration(self):
+        self._duration = None
+        self._sample_end = None
+
+    @property
+    def sample_start(self):
+        if self._sample_start is None:
+            self._sample_start = self.time_start * self.sample_rate
+        return self._sample_start
+
+    @property
+    def sample_end(self):
+        if self._sample_end is None:
+            self._sample_end = self.sample_start + self.sample_rate * self.duration
+        return self._sample_end
+
+    @property
+    def frame_length(self):
+        return self._frame_length
+
+    @frame_length.setter
+    def frame_length(self, new_length):
+        self._frame_length = new_length
+
+    @frame_length.deleter
+    def frame_length(self):
+        self._frame_length = None
+
+    @property
+    def n_fft(self):
+        return self._n_fft
+
+    @n_fft.setter
+    def n_fft(self, new_n_fft):
+        self._n_fft = new_n_fft
+
+    @n_fft.deleter
+    def n_fft(self):
+        self._n_fft = None
+
+    @property
+    def hop_length(self):
+        return self._hop_length
+
+    @hop_length.setter
+    def hop_length(self, new_hop_length):
+        self._hop_length = new_hop_length
+
+    @hop_length.deleter
+    def hop_length(self):
+        self._hop_length = None
 
     def convert_mp3_to_wav(self):
         import audioread
@@ -62,69 +180,51 @@ class AudioFeatures:
         audio_signal, sample_rate = librosa.load(wav_file, sr=self.sample_rate)
         return audio_signal, sample_rate
 
-    def extract_rsme(self, audio_signal=None, frame_length=2048, hop_length=512, sample_start=0, sample_end=None):
-        if sample_end is None:
-            sample_end = self.num_samples
+    def extract_rsm(self):
+        rms = librosa.feature.rms(y=self.audio_signal[self.sample_start:self.sample_end],
+                                  frame_length=self.frame_length,
+                                  hop_length=self.hop_length)
+        return rms
 
-        rmse = librosa.feature.rms(y=audio_signal[sample_start:sample_end], frame_length=frame_length,
-                                   hop_length=hop_length)
-        return rmse
-
-    def extract_zero_crossing_rate(self, audio_signal=None, frame_length=2048, hop_length=512, sample_start=0,
-                                   sample_end=None):
-        if sample_end is None:
-            sample_end = self.num_samples
-        zero_crossing_rate = librosa.feature.zero_crossing_rate(y=audio_signal[sample_start:sample_end],
-                                                                frame_length=frame_length, hop_length=hop_length)
+    def extract_zero_crossing_rate(self):
+        zero_crossing_rate = librosa.feature.zero_crossing_rate(y=self.audio_signal[self.sample_start:self.sample_end],
+                                                                frame_length=self.frame_length,
+                                                                hop_length=self.hop_length)
         return zero_crossing_rate
 
-    def extract_spectral_centroid(self, audio_signal=None, sample_rate=None, n_fft=2048, hop_length=512, sample_start=0,
-                                  sample_end=None):
-        if sample_rate is None:
-            sample_rate = self.sample_rate
-        if sample_end is None:
-            sample_end = self.num_samples
-        spectral_centroid = librosa.feature.spectral_centroid(y=audio_signal[sample_start:sample_end], sr=sample_rate,
-                                                              n_fft=n_fft, hop_length=hop_length)
+    def extract_spectral_centroid(self):
+        spectral_centroid = librosa.feature.spectral_centroid(y=self.audio_signal[self.sample_start:self.sample_end],
+                                                              sr=self.sample_rate, n_fft=self.n_fft,
+                                                              hop_length=self.hop_length)
         return spectral_centroid
 
-    def extract_spectral_rolloff(self, audio_signal=None, sample_rate=None, n_fft=2048, hop_length=512,
-                                 roll_percent=0.85, sample_start=0, sample_end=None):
-        if sample_end is None:
-            sample_end = self.num_samples
-        spectral_rolloff = librosa.feature.spectral_rolloff(y=audio_signal[sample_start:sample_end], sr=sample_rate,
-                                                            n_fft=n_fft, hop_length=hop_length,
-                                                            roll_percent=roll_percent)
+    def extract_spectral_rolloff(self, roll_percent):
+        spectral_rolloff = librosa.feature.spectral_rolloff(y=self.audio_signal[self.sample_start:self.sample_end],
+                                                            sr=self.sample_rate, n_fft=self.n_fft,
+                                                            hop_length=self.hop_length, roll_percent=roll_percent)
         return spectral_rolloff
 
-    def extract_spectral_bandwidth(self, audio_signal=None, sample_rate=None, n_fft=2048, hop_length=512, p=2,
-                                   sample_start=0, sample_end=None):
-        if sample_end is None:
-            sample_end = self.num_samples
-        spectral_bandwidth = librosa.feature.spectral_bandwidth(y=audio_signal[sample_start:sample_end], sr=sample_rate,
-                                                                n_fft=n_fft, hop_length=hop_length, p=p)
+    def extract_spectral_bandwidth(self, p=2):
+        spectral_bandwidth = librosa.feature.spectral_bandwidth(y=self.audio_signal[self.sample_start:self.sample_end],
+                                                                sr=self.sample_rate, n_fft=self.n_fft,
+                                                                hop_length=self.hop_length, p=p)
         return spectral_bandwidth
 
-    def extract_spectral_flux(self, audio_signal=None, sample_rate=None, sample_start=0, sample_end=None):
-        if sample_end is None:
-            sample_end = self.num_samples
-        spectral_flux = librosa.feature.spectral_bandwidth(y=audio_signal[sample_start:sample_end], sr=sample_rate)
+    def extract_spectral_flux(self):
+        spectral_flux = librosa.feature.spectral_bandwidth(y=self.audio_signal[self.sample_start:self.sample_end],
+                                                           sr=self.sample_rate)
         return spectral_flux
 
-    def create_wave_plot(self, audio_signal=None, sample_rate=None, sample_start=0, ax=None):
+    def create_wave_plot(self, ax=None):
 
-        wave_plot = lib_disp.waveplot(audio_signal[sample_start:self.num_samples], sr=sample_rate, alpha=0.5,
-                                      x_axis='time', ax=ax)
+        wave_plot = lib_disp.waveshow(y=self.audio_signal[self.sample_start:self.sample_end],
+                                      sr=self.sample_rate, alpha=0.5, x_axis='time', ax=ax)
         return wave_plot
 
-    def create_magnitude_plot(self, audio_signal=None, sample_rate=None, sample_start=0, sample_end=None, f_ratio=0.5,
-                              ax=None):
-        if sample_end is None:
-            sample_end = self.num_samples
-
-        sp = np.fft.fft(audio_signal[sample_start:sample_end])
+    def create_magnitude_plot(self, f_ratio=0.5, ax=None):
+        sp = np.fft.fft(self.audio_signal[self.sample_start:self.sample_end])
         mag = np.absolute(sp)
-        f = np.linspace(0, sample_rate, len(mag))
+        f = np.linspace(0, self.sample_rate, len(mag))
         f_bins = int(len(mag) * f_ratio)
 
         ax.set_xlabel('Hz')
@@ -132,35 +232,26 @@ class AudioFeatures:
 
         return magnitude_plot
 
-    def create_spectrogram(self, audio_signal=None, sample_rate=None, sample_start=0,
-                           sample_end=None, ax=None):
-        if sample_end is None:
-            sample_end = self.num_samples
-        audio_stft = librosa.stft(audio_signal[sample_start:sample_end])
+    def create_spectrogram(self, ax=None):
+        audio_stft = librosa.stft(y=self.audio_signal[self.sample_start:self.sample_end])
         audio_db = librosa.amplitude_to_db(np.abs(audio_stft), ref=np.max)
-        spectrogram = lib_disp.specshow(audio_db, sr=sample_rate, y_axis='linear', x_axis='time', ax=ax)
+        spectrogram = lib_disp.specshow(audio_db, sr=self.sample_rate, y_axis='linear', x_axis='time', ax=ax)
         return spectrogram
 
-    def create_melspec_plot(self, audio_signal=None, sample_rate=None, n_fft=2048, hop_length=512,
-                            sample_start=0,
-                            sample_end=None, ax=None):
-        if sample_end is None:
-            sample_end = self.num_samples
-        mel = librosa.feature.melspectrogram(audio_signal[sample_start:sample_end], sr=sample_rate, n_fft=n_fft,
-                                             hop_length=hop_length)
+    def create_melspec_plot(self, ax=None):
+        mel = librosa.feature.melspectrogram(y=self.audio_signal[self.sample_start:self.sample_end],
+                                             sr=self.sample_rate, n_fft=self.n_fft,
+                                             hop_length=self.hop_length)
         mel_db = librosa.power_to_db(mel, ref=np.max)
-        melspec_plot = lib_disp.specshow(mel_db, sr=sample_rate, x_axis='time', y_axis='mel', ax=ax)
+        melspec_plot = lib_disp.specshow(mel_db, sr=self.sample_rate, x_axis='time', y_axis='mel', ax=ax)
 
         return melspec_plot
 
-    def create_mfcc_plot(self, audio_signal=None, sample_rate=None,
-                         n_fft=2048, hop_length=512, n_mfcc=20, sample_start=0, sample_end=None, ax=None):
-        if sample_end is None:
-            sample_end = self.num_samples
-        mfcc = librosa.feature.mfcc(audio_signal[sample_start:sample_end],
-                                    sr=sample_rate,
-                                    n_fft=n_fft,
-                                    n_mfcc=n_mfcc,
-                                    hop_length=hop_length)
-        mfcc_plot = lib_disp.specshow(mfcc, sr=sample_rate, x_axis='time', ax=ax)
+    def create_mfcc_plot(self, n_mfcc=20, ax=None):
+        mfcc = librosa.feature.mfcc(y=self.audio_signal[self.sample_start:self.sample_end],
+                                    sr=self.sample_rate,
+                                    n_fft=self.n_fft,
+                                    hop_length=self.hop_length,
+                                    n_mfcc=n_mfcc)
+        mfcc_plot = lib_disp.specshow(mfcc, sr=self.sample_rate, x_axis='time', ax=ax)
         return mfcc_plot
